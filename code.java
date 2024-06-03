eac63c83567a891fb32a30f2c00d3a73c2ba77ac
Ravisankar Nageswaran <ravishankar.n@revature.com>
2024-06-03 16:12:12 +0530

    1st commit dev branch

--- code.java
+++ code.java
@@ -1,7 +1,8 @@
 public class code {
     public static void main(String[] args) {
         int i =1;
-        int sum = i+5;
+        int j =10;
+        int sum = i+j;
         System.out.println(sum);
     }
 }
